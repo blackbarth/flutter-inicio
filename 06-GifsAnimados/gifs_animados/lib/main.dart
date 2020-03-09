0ae7aa9d181e156e54d5760f40ebd38cd990054d
LUIS AUGUSTO FERREIRA <blackbarth@outlook.com>
2020-03-09 12:27:20 -0300

    compartilhar gifs

---
+++ 06-GifsAnimados/gifs_animados/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:gifsanimados/ui/home_page.dart';
+
+void main() => runApp(MaterialApp(
+      home: HomePage(),
+      theme: ThemeData(
+          hintColor: Colors.white,
+          primaryColor: Colors.white,
+          inputDecorationTheme: InputDecorationTheme(
+            enabledBorder:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+            focusedBorder:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.amber)),
+            hintStyle: TextStyle(color: Colors.amber),
+          )),
+    ));
