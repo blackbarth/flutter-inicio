e7375e4c037084bf1c47bb89e2c16ddb0ebd72e7
LUIS AUGUSTO FERREIRA <blackbarth@outlook.com>
2020-03-15 15:23:48 -0300

    Projeto Contatos

---
+++ 07-Contatos/contatos/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:contatos/ui/contact_page.dart';
+import 'package:contatos/ui/homepage.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: HomePage(),
+  debugShowCheckedModeBanner: false,
+));
